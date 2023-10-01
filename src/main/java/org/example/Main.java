efdea50e4485fefeb9c77d7180b6ec702c53ffe1
Daniil <d.dubina@mobidev.biz>
2023-10-01 15:44:56 +0300

    homework 12 case2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,13 @@ package org.example;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
 //        Task_1 task_1 = new Task_1();
 //        task_1.startFirstTask();
 
 
-        Task_2 task_2 = new Task_2(15);
+        Task_2 task_2 = new Task_2(33);
         task_2.run();
     }
 }
--- src/main/java/org/example/Task_2.java
+++ src/main/java/org/example/Task_2.java
@@ -3,112 +3,89 @@ package org.example;
 import java.util.*;
 
 public class Task_2 {
+
+    List<String> result = new ArrayList<>();
+    final Object sync = new Object();
     static int counter = 1;
     int value;
-    List<String> result = new ArrayList<>();
-    List<Thread> threads = new ArrayList<>();
 
     public Task_2(final int value) {
         this.value = value;
     }
 
-    Thread thread1 = new Thread(() -> {
-        while (counter <= value) {
-            if (counter % 3 == 0) {
-                result.add("fizz");
-                increment();
-            }
-        }
-    });
-    Thread thread2 = new Thread(() -> {
-        while (counter <= value) {
-            if (counter % 5 == 0) {
-                result.add("buzz");
-                increment();
-            }
-        }
-    });
-    Thread thread3 = new Thread(() -> {
-        while (counter <= value) {
-            if (counter % 3 == 0 && counter % 5 == 0) {
-                result.add("fizzBuzz");
-                increment();
-            }
-        }
-    });
-    Thread thread4 = new Thread(() -> {
-        while (counter <= value) {
-            if (counter % 3 != 0 && counter % 5 != 0) {
-                result.add(String.valueOf(counter));
-                increment();
-            }
-        }
-    });
-
-    public static synchronized void increment() {
+    public static void increment() {
         counter++;
     }
 
-
-    public void run() throws InterruptedException {
-        threads.add(thread1);
-        threads.add(thread2);
-        threads.add(thread3);
-        threads.add(thread4);
-        for (Thread thread : threads) {
-            thread.start();
-        }
-        for (Thread thread : threads) {
-            thread.join();
-        }
-        System.out.println(result);
+    public void run() {
+        new Thread(() -> {
+            synchronized (sync) {
+                while (counter <= value) {
+                    if (!(counter % 3 == 0 && counter % 5 == 0) && counter % 3 == 0) {
+                        result.add("fizz");
+                        increment();
+                        sync.notifyAll();
+                    } else {
+                        try {
+                            sync.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+            System.out.println(result);
+        }).start();
+        new Thread(() -> {
+            synchronized (sync) {
+                while (counter <= value) {
+                    if (!(counter % 3 == 0 && counter % 5 == 0) && counter % 5 == 0) {
+                        result.add("buzz");
+                        increment();
+                        sync.notifyAll();
+                    } else {
+                        try {
+                            sync.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            synchronized (sync) {
+                while (counter <= value) {
+                    if (counter % 3 == 0 && counter % 5 == 0) {
+                        result.add("fizzBuzz");
+                        increment();
+                        sync.notifyAll();
+                    } else {
+                        try {
+                            sync.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            synchronized (sync) {
+                while (counter <= value) {
+                    if (counter % 3 != 0 && counter % 5 != 0) {
+                        result.add(String.valueOf(counter));
+                        increment();
+                        sync.notifyAll();
+                    } else {
+                        try {
+                            sync.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+        }).start();
     }
-
-//    public synchronized void fizz() throws InterruptedException {
-//        while (counter < value) {
-//            if (counter % 3 == 0) {
-//                result.add("fizz");
-//                System.out.println("fizz");
-//                notifyAll();
-//            } else {
-//                wait();
-//            }
-//        }
-//    }
-//
-//    public synchronized void buzz() throws InterruptedException {
-//        while (counter < value) {
-//            if (counter % 5 == 0) {
-//                result.add("buzz");
-//                System.out.println("buzz");
-//                notifyAll();
-//            } else {
-//                wait();
-//            }
-//        }
-//    }
-//
-//    public synchronized void fizzBuzz() throws InterruptedException {
-//        while (counter < value) {
-//            if (counter % 3 == 0 || counter % 5 == 0) {
-//                result.add("fizzBuzz");
-//                System.out.println("fizzBuzz");
-//                notifyAll();
-//            } else {
-//                wait();
-//            }
-//        }
-//    }
-//
-//    public synchronized void number() throws InterruptedException {
-//        while (counter < value) {
-//            if (counter % 3 != 0 && counter % 5 != 0) {
-//                result.add(String.valueOf(counter));
-//                System.out.println(counter);
-//                notifyAll();
-//            } else {
-//                wait();
-//            }
-//        }
-//    }
 }
